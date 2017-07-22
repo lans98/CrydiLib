b5d922cafae3136ade00a07e98b87e40914ffa63
lans98 <lans9831@gmail.com>
2017-07-22 15:00:33 +0000

    new functions added

---
+++ build/CMakeFiles/crydi3_strings_bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crydi3_strings_bin.dir/test/strings_test.cc.o"
+  "crydi3_strings_bin.pdb"
+  "crydi3_strings_bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/crydi3_strings_bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
