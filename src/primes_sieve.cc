11fbb16f4da3f4588149e7372e54fa08e4cc2608
Kevin <lans9831@gmail.com>
2017-11-23 15:16:45 -0500

    gitignore

--- src/primes_sieve.cc
+++ src/primes_sieve.cc
@@ -3,24 +3,24 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-                                                                      
+
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-                                                                      
+
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// ========================================================================= 
+// =========================================================================
 
-// ========================================================================= 
+// =========================================================================
 // Disclaimer notes:
 //  This library is not intended to be a viable option for use in situations
-//  of reliability and robustness, several of these algorithms need to be 
+//  of reliability and robustness, several of these algorithms need to be
 //  better implemented, this library serves to demonstrate certain algorithms
-//  applied to the theory of numbers, also serves as an example as a basis 
+//  applied to the theory of numbers, also serves as an example as a basis
 //  for creating a library (quite simple).
-// ========================================================================= 
+// =========================================================================
 
 #include "inc/primes_sieve.h"
 
@@ -42,12 +42,12 @@ void PrimesSieve::RegularSieve() {
 	if (flags & SEG_SIEVE) delete[] barray;
 	barray = new BitArray<>(limit + 1, -1);
 
-	// Solo se recorre hasta sqrt(n) ya que no habra ningun 
+	// Solo se recorre hasta sqrt(n) ya que no habra ningun
 	// multiplo mayor de sqrt(n) que filtrar en la criba
 	for (uintmax_t i = 2; i <= sqrt_limit; ++i) {
 		// Si fue marcado como compuesto vamos a la siguiente iteración
 		if (!barray->At(i)) continue;
-		// Si es primo marquemos todos los multiplos de i que caben 
+		// Si es primo marquemos todos los multiplos de i que caben
 		// en la criba.
 		for (uintmax_t j = i * i; j <= limit; j += i) {
 			barray->Set(j, 0);
@@ -55,7 +55,7 @@ void PrimesSieve::RegularSieve() {
 	}
 
 	// Cambia las banderas, quitamos que fue hecha con el metodo de criba
-	// segmentada, y configuramos las flags para saber que fue hecho con 
+	// segmentada, y configuramos las flags para saber que fue hecho con
 	// el metodo de criba regular
 	flags &= ~SEG_SIEVE;
 	flags |=  REG_SIEVE;
@@ -72,15 +72,15 @@ void PrimesSieve::RegularSieve() {
 void PrimesSieve::SegmentedSieve() {
 	uintmax_t sqrt_limit = static_cast<uintmax_t>(sqrt(limit));
 	uintmax_t segment_size = sqrt_limit;
-	
+
 	if (flags & REG_SIEVE) delete barray;
 	barray = new BitArray<>[2]{
 		// BitArray que contendra los primos más pequeños menores
 		// que sqrt(n), acceder con 0
 		BitArray<>(sqrt_limit + 1, -1),
 
-		// BitArray representado un segmento de criba de tamaño de 
-		// segment_size, usado multiples veces durante todo el 
+		// BitArray representado un segmento de criba de tamaño de
+		// segment_size, usado multiples veces durante todo el
 		// proceso (osea no se crean más segmento, este es utilizado
 		// siempre).
 		BitArray<>(segment_size, -1),
@@ -97,7 +97,7 @@ void PrimesSieve::SegmentedSieve() {
 	}
 
 	for (uintmax_t low = 0; low <= limit; low += segment_size) {
-	
+
 	}
 }
 
